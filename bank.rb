ddba15af6927eafd2a38bcab71871d315c5b04f0
danclark1820 <danclark1820@gmail.com>
2014-03-06 11:32:03 -0500

    completed the challange..its ugly

--- bank.rb
+++ bank.rb
@@ -1,65 +1,40 @@
-# OOP Fundamentals 1
-# Bank transactions challenge
-
 require 'csv'
 require 'pry'
 
-
-# data import
 bank_data = []
 CSV.foreach('bank_data.csv', headers: true) do |row|
   row["Amount"] = row["Amount"].to_f
   bank_data << row.to_hash
 end
 
-# data import
 balances = []
 CSV.foreach('balances.csv', headers: true) do |row|
   row["Balance"] = row["Balance"].to_f
   balances << row.to_hash
 end
 
-# bank_data = [
-# {"Date"=>"9/12/2013", "Amount"=>-493.0, "Description"=>"Awesome Coffee Inc.", "Account"=>"Purchasing Account"}
-# {"Date"=>"9/14/2013", "Amount"=>5041.39, "Description"=>"Sales Deposit", "Account"=>"Purchasing Account"}
-# {"Date"=>"9/18/2013", "Amount"=>-28.0, "Description"=>"Pizza Pizza", "Account"=>"Business Checking"}
-# {"Date"=>"9/20/2013", "Amount"=>49.5, "Description"=>"Quickbooks Pro Subscription", "Account"=>"Business Checking"}
-# {"Date"=>"9/21/2013", "Amount"=>-693.0, "Description"=>"Awesome Coffee Inc.", "Account"=>"Purchasing Account"}
-# {"Date"=>"9/21/2013", "Amount"=>3033.21, "Description"=>"Sales Deposit", "Account"=>"Purchasing Account"}
-# {"Date"=>"9/28/2013", "Amount"=>2200.39, "Description"=>"Sales Deposit", "Account"=>"Business Checking"}
-# {"Date"=>"9/29/2013", "Amount"=>-750.0, "Description"=>"Nick's Pastry", "Account"=>"Purchasing Account"}
-# {"Date"=>"9/30/2013", "Amount"=>-200.0, "Description"=>"Awesome Cofee Inc.", "Account"=>"Purchasing Account"}
-# {"Date"=>"10/1/2013", "Amount"=>-19.99, "Description"=>"Staples, Inc.", "Account"=>"Business Checking"}
-# {"Date"=>"10/2/2013", "Amount"=>-29.99, "Description"=>"Amazon.com", "Account"=>"Business Checking"}
-# ]
-
-
-# balances = [
-# {"Account"=>"Business Checking",  "Balance"=>4010.0},
-# {"Account"=>"Purchasing Account", "Balance"=>2030.0}
-# ]
-
 class BankTransaction
 
-  attr_reader :date, :amount, :description, :account,
+  attr_reader :date, :amount, :description, :account, :hash
 
   def initialize(hash)
     @date = hash["Date"]
     @amount = hash["Amount"]
     @description = hash["Description"]
     @account = hash["Account"]
+    @amount_type
   end
 
   def credit_or_debit
-    if amount > 0
-      @amount_type = "credit"
+    if self.amount > 0
+       "credit"
     else
-      @amount "debit"
+       "debit"
     end
   end
 
   def summary
-    "#{@amount.abs} #{@amount_type} #{date} - #{@description}"
+    "$#{"%0.2f" % @amount.abs} #{self.credit_or_debit} #{date} - #{@description}"
   end
 
 end
@@ -70,16 +45,15 @@ bank_data.each do |trans|
 end
 
 
-
 class BankAccount
 
-  attr_reader :starting_bal, :account , :ending_bal
+  attr_reader :hash, :ending_balance, :name, :starting_balance
 
-  def initialize(hash, account, description)
+  def initialize(hash, array)
+    @name = hash["Account"]
     @starting_balance = hash["Balance"]
-    @account = account
-    @ending_balance= hash["Balance"]
-    @description = description
+    @array = array
+    @ending_balance = self.ending_balance
   end
 
   def starting_balance
@@ -87,16 +61,41 @@ class BankAccount
   end
 
   def ending_balance
-    @ending_balance += @starting_balance
+    start_b = @starting_balance
+    @array.each do |trans_obj|
+      if trans_obj.account == name
+        start_b += trans_obj.amount
+      end
+    end
+    @ending_balance = start_b
   end
 
-  # def summary
-  #   summaries = []
-  #   transactions_array.each do |trans|
-  #     summaries << trans.summary
-  #   end
-  #   summaries
-  # end
+
+  def summary
+    summaries = []
+    @array.each do |trans_obj|
+      if trans_obj.account == name
+        summaries<<(trans_obj.summary)
+      end
+    end
+    summaries
+  end
+
+end
+
+bas = []
+balances.each do |ba|
+  bas<<BankAccount.new(ba, transactions_array)
+end
+
+bas.each do |acct|
+  puts "=== #{acct.name} ==="
+  puts "Starting balance: $#{"%0.2f" % acct.starting_balance}"
+  puts "Ending balance:   $#{"%0.2f" % acct.ending_balance} \n "
+  acct.summary.each do |sum|
+    puts sum
+  end
+  puts "======="
 end
 
 
